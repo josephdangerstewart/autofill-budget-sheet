18d7fbf32f4d37e917eb7c01e75c350aed0131bd
Joseph Stewart <joseph.william.stewart@biola.edu>
2021-11-10 22:41:57 -0800

    Refactor logic that puts data in sheet

---
+++ addTransactionsToSheet.ts
@@ -0,0 +1,27 @@
+import { getAccessToken, getTransactions } from './plaid';
+import { getMostRecentTransactionDate, getRules, recordClassificationResults } from './google';
+import { subMonths } from 'date-fns';
+import { tryClassifyTransactions } from './classifier';
+
+export async function addTransactionsToSheet() {
+	const [
+		plaidAccessToken,
+		lastTransactionDate,
+		rules
+	] = await Promise.all([
+		getAccessToken(),
+		getMostRecentTransactionDate(),
+		getRules(),
+	]);
+
+	console.log(lastTransactionDate);
+
+	const transactions = await getTransactions(
+		plaidAccessToken,
+		lastTransactionDate ?? subMonths(new Date, 3)
+	);
+
+	const classifications = tryClassifyTransactions(transactions, rules);
+
+	await recordClassificationResults(classifications);
+}
--- index.ts
+++ index.ts
@@ -1,35 +1,10 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { subMonths } from 'date-fns';
-import { getAccessToken, getTransactions } from './plaid';
-import { getMostRecentTransactionDate, getRules, recordClassificationResults } from './google';
-import { tryClassifyTransactions } from './classifier';
-
+import { addTransactionsToSheet } from './addTransactionsToSheet';
 
 async function main() {
-	const [
-		plaidAccessToken,
-		lastTransactionDate,
-		rules
-	] = await Promise.all([
-		getAccessToken(),
-		getMostRecentTransactionDate(),
-		getRules(),
-	]);
-
-	console.log(lastTransactionDate);
-
-	const transactions = await getTransactions(
-		plaidAccessToken,
-		lastTransactionDate ?? subMonths(new Date, 3)
-	);
-
-	const classifications = tryClassifyTransactions(transactions, rules);
-
-	await recordClassificationResults(classifications);
-
-	process.exit(0);
+	await addTransactionsToSheet()
 }
 
 main();
