442a41daea1ff0c43f21dd09773afc34898dcf4f
Joseph Stewart <joseph.william.stewart@biola.edu>
2021-11-13 11:31:05 -0800

    Get all data from transaction sheets and calculate most recent date that way instead of doing the separate sheet thing

--- google/getMostRecentTransactionDate.ts
+++ google/getMostRecentTransactionDate.ts
@@ -4,12 +4,37 @@ import { sheetInfo } from './sheetInfo';
 import { getSheetData } from './sheetManipulation';
 
 export async function getMostRecentTransactionDate(): Promise<Date | null> {
-	const response = await getSheetData(sheetInfo.sheets.transactions, 1);
-	const allDates = response
-		?.map((x)=> x.date && parse(x.date, 'yyyy-MM-dd', new Date()))
-		.filter(x => x && !isNaN(x.getTime()));
+	const [
+		classifiedTransactionsResponse,
+		incomeTransactionsResponse,
+		manualReviewTransactionsResponse,
+	] = await Promise.all([
+		getSheetData(sheetInfo.sheets.transactions, 1),
+		getSheetData(sheetInfo.sheets.income, 1),
+		getSheetData(sheetInfo.sheets.needsManualReview, 1),
+	]);
 
-	if (!allDates?.length) {
+	const transactionDates = classifiedTransactionsResponse
+		?.filter((x) => x.wasAutoAdded === 'YES' && x.id)
+		.map((x) => x.date) ?? [];
+	
+	const incomeDates = incomeTransactionsResponse
+		?.filter((x) => x.wasAutoAdded === 'YES' && x.id)
+		.map((x) => x.date) ?? [];
+	
+	const manualReviewDates = manualReviewTransactionsResponse
+		?.filter((x) => x.id)
+		.map((x) => x.date) ?? [];
+
+	const allDates = [
+		...transactionDates,
+		...incomeDates,
+		...manualReviewDates
+	]
+	.map((x)=> x && parse(x, 'yyyy-MM-dd', new Date()))
+	.filter(x => x && !isNaN(x.getTime()))
+
+	if (!allDates.length) {
 		return null;
 	}
 
